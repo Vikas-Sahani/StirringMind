30a4811d1a13d87cde34512784657e50dcbdca28
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2023-07-27 14:59:22 +0530

    again starting with backend

--- backend/db/conn.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const db = process.env.DATABASE;
-mongoose
-  .connect(db)
-  .then(() => console.log("connection is succesfull"))
-  .catch(() => {
-    console.log("connection is not succesfull");
-  });
