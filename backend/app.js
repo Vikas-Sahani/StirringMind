c09dc9058c1916486f972e1a62b07d2b63b33e1e
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2023-07-27 15:39:12 +0530

    basic router is done & when we don't exprot it then we got app.use() error

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 require("dotenv").config();
 require("./db/conn");
-const app = express();
+const router = require("./routes/router");
 
-const port = process.env.PORT;
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(router);
 
 app.get("/", (req, res) => {
   res.send("hello from the backend");
 });
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log(`server is connected at ${port}`);
 });
